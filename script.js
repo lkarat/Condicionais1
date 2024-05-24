ffd5821b2e68abbd66e6fd9121700305da85cabc
Luiz Karat <luiz.karat.silva@gmail.com>
2024-05-23 21:43:12 -0300

    Aula de Condicional 1

---
+++ script.js
@@ -0,0 +1,40 @@
+//condidionais 1
+//Função que receba parâmetro
+
+/*function verificarBoleano(parametro) {
+    if(!parametro) {
+        console.log("Tô dentro do if");
+
+
+    }else{
+        console.log("Tô dentro do else");
+
+
+    }
+
+}
+verificarBoleano(undefined)*/
+
+/*function autorizaCnh(idade) {
+    idade = Number(prompt("Qual é a sua idade?"))
+
+    if (idade >= 18) {
+        console.log("Você é maior de idade e pode dirigir!");
+
+    }else if (idade<18 && idade>= 16) {
+        console.log("Pode dirigir com autorization");
+    }else {
+        console.log("Você ainda não pode dirigir, espere mais um pouco!");
+    }
+}
+autorizaCnh()*/
+
+function trabalhaNoLaBank (cliente,filhos,idadesFilhos) {
+    cliente
+    if (cliente) {
+        
+    }
+
+
+
+}
